1b8f26606d7b4f6c3fb711758581c3f840f50baf
kyungmin <kyungmin@fairsquarelab.com>
2024-03-22 15:28:37 +0900

    0. Node-Template Initial Commit

--- start.sh
+++ start.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-INSTALL_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. $INSTALL_ROOT/node_config
+#INSTALL_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#. $INSTALL_ROOT/node_config
 
-LOG_NAME="$(basename $INSTALL_ROOT)"
-if [[ "$LOCALIZE_LOG" == "true" ]]; then
-  LOG_PATH="$INSTALL_ROOT/nodeos.log"
-else
-  LOG_PATH="/var/log/nodeos/$LOG_NAME.log"
-fi
+#LOG_NAME="$(basename $INSTALL_ROOT)"
+#if [[ "$LOCALIZE_LOG" == "true" ]]; then
+#  LOG_PATH="$INSTALL_ROOT/nodeos.log"
+#else
+#  LOG_PATH="/var/log/nodeos/$LOG_NAME.log"
+#fi
 
-if [[ "$DATA_DIR" != "" ]]; then
-  DATA_DIR_PATH=$DATA_DIR
-else
-  DATA_DIR_PATH=$INSTALL_ROOT/data
-fi
+#if [[ "$DATA_DIR" != "" ]]; then
+#  DATA_DIR_PATH=$DATA_DIR
+#else
+#  DATA_DIR_PATH=$INSTALL_ROOT/data
+#fi
 
-nohup $NODEOS_BIN --disable-replay-opts --data-dir $DATA_DIR_PATH --config-dir $INSTALL_ROOT "$@" >> "$LOG_PATH" 2>&1 &
+nohup nodeos --disable-replay-opts --data-dir ./data --config-dir . "$@" >> "nodeos.log" 2>&1 &
 PID="$!"
 echo "nodeos started with pid $PID"
 echo $PID > $INSTALL_ROOT/nodeos.pid
