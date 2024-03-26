798cfb6195649126bba55ebca1fddd353f3ef3cb
kyungmin <kyungmin@fairsquarelab.com>
2024-03-26 12:00:27 +0900

    Update

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@
 #  DATA_DIR_PATH=$INSTALL_ROOT/data
 #fi
 
-nohup nodeos --disable-replay-opts --data-dir ./data --config-dir . >> "nodeos.log" 2>&1 &
+nohup nodeos --disable-replay-opts --enable-stale-production --data-dir ./data --config-dir . >> "nodeos.log" 2>&1 &
 PID="$!"
 echo "nodeos started with pid $PID"
 echo $PID > $INSTALL_ROOT/nodeos.pid
